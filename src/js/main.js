e78a2882903f478f03204f96fd30fff4c24af882
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2024-02-22 15:00:15 +0300

    Release 1.0
    Theme toggle switch added
    Transition is applied
    JS functions as intended

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-import "../scss/style.scss"
+import "../scss/style.scss";
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const checkbox = document.querySelector('#special');
+    
+    const changeTheme = () => {
+        const page = document.querySelector('.page');
+
+        if (checkbox.checked) {
+            page.classList.remove('him');
+            page.classList.add('her');    
+        } else {
+            page.classList.remove('her');
+            page.classList.add('him');
+        }
+    };
+
+    checkbox.addEventListener('change', () => {
+        changeTheme();
+    });
+
+});
